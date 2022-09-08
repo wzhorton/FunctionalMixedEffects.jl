5664bf3812d3f7a8a2a2e79b21ef4e5c5f91f824
Zach Horton <wz.horton@gmail.com>
2022-09-08 11:48:12 -0700

    package skeleton

---
+++ src/FunctionalMixedEffects.jl
@@ -0,0 +1,16 @@
+module FunctionalMixedEffects
+
+include("GeneralTools.jl")
+
+using LinearAlgebra
+using Distributions
+using ProgressMeter
+import .GeneralTools as Tools
+
+
+
+
+
+
+
+end # module
---
+++ src/GeneralTools.jl
@@ -0,0 +1,18 @@
+# General tools used repeatedly with more general application
+
+module GeneralTools
+
+#======================================
+Template function description
+
+Arguments
+x : Input Type
+
+Note:
+
+Return:
+Exported: true/false ==================#
+
+
+
+end # module
