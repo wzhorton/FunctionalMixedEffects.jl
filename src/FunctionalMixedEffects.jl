d362a3af028a0a8b65cafd1ffd01250960e09416
Zach Horton <wz.horton@gmail.com>
2022-09-12 16:50:42 -0700

    outsource matrix utilities

--- src/FunctionalMixedEffects.jl
+++ src/FunctionalMixedEffects.jl
@@ -4,58 +4,16 @@ using LinearAlgebra
 using Distributions
 using BSplines
 
-#--------------------------#
-# General Helper Functions #
-#--------------------------#
-function first_order_penalty_mat(p::Integer)
-    @assert p >= 4
-    pmat = zeros(Float64, p, p)
-    for i in Base.OneTo(p-1)
-        pmat[i,i] = 2.
-        pmat[i+1,i] = -1.
-        pmat[i,i+1] = -1.
-    end
-    pmat[p,p] = 1.
-    return Hermitian(pmat)
-end
+include("MatrixUtils.jl")
+import .MatrixUtils as mutil
 
-function simple_bspline_design_mat(x, low_bound, up_bound,  p::Integer)
-    @assert p >= 4
-    @assert low_bound < up_bound
-    knots = range(low_bound, up_bound, p-2)
-    basis = BSplineBasis(4, knots)
-    return basismatrix(basis, x)
-end
+
+#------------------#
+# Helper Functions #
+#------------------#
 
 function identity_mat(::Type{T}, n::Integer) where {T <: Real}
-    Matrix{T}(I, n, n)
-end
-
-function conjugate_matrix_normal_regression(Y,X,U,V,C,D)
-    #= Model parameters:
-    Y (pxn) ~ MN(B*X, U, V)
-    B (pxq) ~ MN(M, C, D)
-    This draws a posterior value for B 
-    Note that no validation code is used to
-    either verify correct matrix formats or 
-    match dimensions, other than what is
-    implied by the operations themselves. 
-    
-    Also note that many straight inverses in this formula can't be avoided.
-    For the sake of readability, only one system solve is used=#
-
-    # I think the need for this is a bug. I've submitted a GitHub issue
-    Cinv = typeof(C) <: Diagonal ? inv(C) : inv(cholesky(C))
-    Dinv = typeof(D) <: Diagonal ? inv(D) : inv(cholesky(D))
-    Uinv = typeof(U) <: Diagonal ? inv(U) : inv(cholesky(U))
-    VinvXt = typeof(V) <: Diagonal ? V \ X' : cholesky(V) \ X'
-
-    C_post = typeof(C) <: Diagonal && typeof(U) <: Diagonal ? inv(Uinv + Cinv) : inv(cholesky(Uinv + Cinv))
-    D_post = inv(cholesky(X * VinvXt + Dinv))
-    M_post = C_post * (Uinv * Y * VinvXt + Cinv * M * Dinv) * D_post
-
-    Z = rand(Normal(), size(M_post))
-    return M_post + cholesky(C_post).L * Z * cholesky(D_post).U
+    Diagonal(Matrix{T}(I, n, n))
 end
 
 
---
+++ src/MatrixUtils.jl
@@ -0,0 +1,94 @@
+# Matrix utility tools
+
+module MatrixUtils
+
+using Distributions
+using LinearAlgebra
+
+
+#===================================================================================
+First order penalty matrix for P-splines
+
+Arguments:
+p : Integer
+
+Return: A Hermitian matrix.
+Note: Although the structure here supports a symmetric tri-diagonal matrix, the way 
+this matrix is usually used will only preserve the Hermitian type.
+Exported: true =====================================================================#
+export first_order_penalty_mat
+
+function first_order_penalty_mat(p::Integer)
+    @assert p >= 4
+    pmat = zeros(Float64, p, p)
+    for i in Base.OneTo(p-1)
+        pmat[i,i] = 2.
+        pmat[i+1,i] = -1.
+        pmat[i,i+1] = -1.
+    end
+    pmat[p,p] = 1.
+    return Hermitian(pmat)
+end
+
+
+#======================================================
+Cubic B-spline design matrix for evenly spaced knots
+
+Arguments:
+x : vector of evaluation locations
+low_bound/up_bound : bounds for the vector x
+p : number of spline coefficients
+
+Return: Dense matrix with row evaluations.
+Exported: true =======================================#
+export simple_bspline_design_mat
+
+function simple_bspline_design_mat(x, low_bound, up_bound,  p::Integer)
+    @assert p >= 4
+    @assert low_bound < up_bound
+    knots = range(low_bound, up_bound, p-2)
+    basis = BSplineBasis(4, knots)
+    return basismatrix(basis, x)
+end
+
+
+#=====================================================================================
+Conjugate matrix-normal matrix-normal regression coefficients
+
+Arguments:
+Y, X : Observation matrices
+U, V, C, D : Covariance Matrices
+
+Notes:
+Model parameters follow this structure:
+    Y (pxn) ~ MN(B*X, U, V)
+    B (pxq) ~ MN(M, C, D)
+Dimensions are checked implicitly. Diagonal matrices are handled separately due to
+Cholesky compatibility. Other special types may not work, but Hermitian does work.
+Against common advice, this function caches inverted matrices rather than solving
+a the linear system each time. This is required in several places, so for the
+sake of readability has been done in all cases.
+
+Returns: MatrixNormal object for posterior B|Y
+Exported: true ======================================================================#
+export conjugate_matrix_normal_regression
+
+function conjugate_matrix_normal_regression(Y,X,U,V,C,D)
+    # I think the need for this is a bug. I've submitted a GitHub issue
+    Cinv = typeof(C) <: Diagonal ? inv(C) : inv(cholesky(C))
+    Dinv = typeof(D) <: Diagonal ? inv(D) : inv(cholesky(D))
+    Uinv = typeof(U) <: Diagonal ? inv(U) : inv(cholesky(U))
+    VinvXt = typeof(V) <: Diagonal ? V \ X' : cholesky(V) \ X'
+
+    C_post = typeof(C) <: Diagonal && typeof(U) <: Diagonal ? inv(Uinv + Cinv) : inv(cholesky(Uinv + Cinv))
+    D_post = inv(cholesky(X * VinvXt + Dinv))
+    M_post = C_post * (Uinv * Y * VinvXt + Cinv * M * Dinv) * D_post
+
+    return MatrixNormal(M_post, C_post, D_post)
+end
+
+
+
+
+
+end # module
