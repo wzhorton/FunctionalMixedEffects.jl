a42be6384b71dfa7f24dc1b339aa8becd959eb26
Zach Horton <wz.horton@gmail.com>
2022-09-15 09:52:40 -0700

    update test file for new structure

--- test/test.jl
+++ test/test.jl
@@ -1,15 +1,18 @@
 include("../src/FunctionalMixedEffects.jl")
 using .FunctionalMixedEffects
 using Distributions
+using LinearAlgebra
 
 x = collect(1:101)
 Y = hcat([i .+ rand(Normal(0, 0.002),101) for i in 1:10]...)
 
-Xfix = reshape(repeat([1.],10), 1, 10)
+Xcent = reshape(repeat([1.],5), 1, 5)
+Xrand = kron(Diagonal(Matrix(1.0I, 5, 5)), [1.0 1.0])
 
 hyps = HyperParametersFME()
-cfg = OutputConfigFME(15000,5000,1,false,true)
+cfg = OutputConfigFME(20, 15000,5000,1,false,true)
+data = DataFME(Y, nothing, Xrand, Xcent)
 
-chains = mcmc_fme(Y, Xfix, nothing, 20, hyps, cfg);
+chains = mcmc_fme(data, hyps, cfg);
 println(mean(chains.σ))
 println(mean(chains.τ))
